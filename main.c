da42d4f7dfafb82d0ba94c45f8c4fc5d2eddfc38
ojlogic <wojlogic@gmail.com>
2024-04-06 19:50:01 +0300

    learning

--- main.c
+++ main.c
@@ -1,49 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 
-char conv(char value) {
-    return (value >= '0' && value <= '9') ? value : value + 128;
-}
+double sumf(const char* format, ...);
 
-int match_ab(const char a, const char b) {
-    int res = 0;
-    if ((a >= '0' && a <= '9') && (b == ' ' || b == '\0'))
-        res = 1;
-    else if ((b >= '0' && b <= '9') && (a == ' ' || a == '\0'))
-        res = 0;
-    else
-        res = a < b;
-    return res;
-}
-
-
-void sort_string(char *str, int max_len, int (*ptr_criterion)(const char, const char)) {
-    for (int i = 0; i < max_len; i++) {
-        char min = str[i];
-        int index = i, j = i + 1;
-        for (; j < max_len; j++) {
-            if ((*ptr_criterion)(str[j], min)) {
-                min = str[j];
-                index = j;
-            }
-        }
-        if (index != i) {
-            char temp = str[i];
-            str[i] = str[index];
-            str[index] = temp;
-        }
-    }
-}
-
-int main(void) {
-    char str[100] = {0};
-    fgets(str, sizeof(str) - 1, stdin);
-    char *ptr_n = strrchr(str, '\n');
-    if (ptr_n != NULL)
-        *ptr_n = '\0';
-
-    sort_string(str, strlen(str), match_ab);
-    puts(str);
+int main(void)
+{
+    double res = sumf("++ + +", 1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+    printf("%.2f ", res);
 
     return 0;
+}
+
+double sumf(const char* format, ...) {
+    double res = 0.0, current;
+    va_list arg;
+    va_start(arg, format);
+    for(int i = 0; i < (int)strlen(format); i++) {
+        current = (double)va_arg(arg, double); 
+        if('+' == format[i]) {
+            res += current;
+            //printf("i = %d, current = %.2lf, sign = %c\n", i, current, format[i]);
+        }        
+    }
+    va_end(arg);
+    return res;
 }
